57197e8c13825bf24bbf93807b1eac53747dd67a
ikennaex <ikennaexcel@gmail.com>
2025-09-06 11:23:42 +0100

    authToken added to route hander

--- middleware/auth.js
+++ middleware/auth.js
@@ -15,7 +15,6 @@ const authToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
-    console.log("Decoded JWT:", decoded);
     req.user = decoded; 
     next();
   } catch (error) {
--- routes/errandRoutes.js
+++ routes/errandRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const { postErrand, getAllErrands, getErrandById, deleteErrand, editErrand } = require('../controllers/ErrandController')
+const { authToken, checkOwnership } = require('../middleware/auth')
 const router = express.Router()
 
-router.post("/", postErrand)
+router.post("/", authToken, postErrand)
 router.get("/", getAllErrands)
 router.get("/:id", getErrandById)
-router.delete("/:id", deleteErrand)
-router.put("/:id", editErrand)
+router.delete("/:id", checkOwnership, deleteErrand)
+router.put("/:id", checkOwnership, editErrand)
 
 module.exports = router;
