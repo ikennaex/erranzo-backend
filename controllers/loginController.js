9c8883b3898388fdfa81173ce18acec835d42711
ikennaex <ikennaexcel@gmail.com>
2025-09-06 12:26:25 +0100

    cookie settings refresh token

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -37,8 +37,8 @@ const login = async (req, res) => {
     // Send refresh token in HttpOnly cookie
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "None",
+      secure: process.env.NODE_ENV === "production", // true only on HTTPS
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
