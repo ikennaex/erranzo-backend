5d2d84e3289f268ba3a0d5980cf80a6ead78ef69
ikennaex <ikennaexcel@gmail.com>
2025-09-24 21:04:01 +0100

    hot fix

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,9 +3,9 @@ const { getUserProfile, updateUserProfile, getAllErrandsPosted, getAllAcceptedEr
 const {authToken, checkOwnership} = require('../middleware/auth')
 const router = express.Router()
 
-router.get('/:id', getUserProfile)
 router.get('/allpostederrands', authToken, getAllErrandsPosted)  // get all errands posted by a user 
 router.get('/allacceptederrands', authToken, getAllAcceptedErrands)  // get all errands posted by a user 
+router.get('/:id', getUserProfile)
 router.put('/:id', authToken, checkOwnership, updateUserProfile)
 
 
