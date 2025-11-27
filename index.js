dca9eb0f0c7a5f32287a7491a3ddc3097e43428e
ikennaex <ikennaexcel@gmail.com>
2025-11-27 14:21:50 +0100

    update: fetch chats

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://erranzo.onrender.com",
+      "https://erranzo.vercel.app"
     ],
     credentials: true,
   })
