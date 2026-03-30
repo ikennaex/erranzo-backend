31d180de87fd5ad48ce214aa2d317d2f8cf39f83
ikennaex <ikennaexcel@gmail.com>
2026-03-30 16:57:29 +0100

    update: search

--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -2,6 +2,6 @@ const express = require('express')
 const { handleSearch } = require('../controllers/searchController')
 const router = express.Router()
 
-router.get('/search', handleSearch) 
+router.get('/', handleSearch) 
 
 module.exports = router 
