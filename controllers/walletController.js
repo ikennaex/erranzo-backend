e68527dbd00ad9c88feb0dbbcd02a5eed4f8bdb7
ikennaex <ikennaexcel@gmail.com>
2026-05-14 09:21:16 +0100

    update: get wallet balance

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -1,7 +1,7 @@
 const Wallet = require("../models/Wallet");
 
 const getWallet = async (req, res) => {
-  const wallet = await Wallet.findOne({ userId: req.params.userId });
+  const wallet = await Wallet.findOne({ userId: req.user.id });
 
   res.json(wallet);
 };
--- routes/walletRoute.js
+++ routes/walletRoute.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const { getWallet } = require("../controllers/walletController");
+const { authToken } = require("../middleware/auth");
 
 
-router.get("/:userId", getWallet);
+router.get("/me", authToken, getWallet);
 
 module.exports = router;
